f0e01306868125a9e6638bbf1c4b18e735e3345e
Mohit Jangid <77240127+Mohitjangid097@users.noreply.github.com>
2022-08-02 02:27:23 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content="Product Rental Website Rentweb" />
-    <meta name="author" content="Mukul Saini" />
+    <meta name="author"  />
         <title>SignIn||Rentweb</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
