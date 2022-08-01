9edaa19dbe103c239d8e1d545bd6823f0d8fd129
mohit jangid <mohitjangid183@gmail.com>
2022-08-02 01:13:33 +0530

    first commit

---
+++ db/db.php
@@ -0,0 +1,10 @@
+<?php 
+
+$server = "sql212.epizy.com";
+$username = "epiz_27429826";
+$password = "l5GrKhJ75CN74BI";
+$dbname = "epiz_27429826_rentweb";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+?>
